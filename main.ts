4f6a0d5020a14088a5e044ebcb35a0941fe98ea7
Yuta <yuta.marines.team26l@gmail.com>
2024-01-31 16:37:32 +0900

    first commit

---
+++ main.ts
@@ -0,0 +1,48 @@
+declare var Highcharts: any;
+
+document.addEventListener('DOMContentLoaded', function () {
+  const chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'container',
+            defaultSeriesType: 'line',
+            margin: [50, 150, 60, 80]
+        },
+        title: {
+            text: '平均CPM',
+            style: {margin: '50px 100px 0 0'}
+        },
+        xAxis: {
+            categories: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            title: {text: '月'}
+        },
+        yAxis: {
+            title: {text: 'CPM (¥)'},
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            formatter: function(this: any) {
+                return this.series.name + ' ' + this.x + ': ¥' + this.y;
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -10,
+            y: 100,
+            borderWidth: 0
+        },
+        series: [{
+            name: '広告枠A',
+            data: [75, 82, 80, 76, 72, 70, 74, 79, 77, 81, 73, 75]
+        }, 
+        {
+            name: '広告枠B',
+            data: [65, 68, 74, 71, 73, 66, 80, 71, 69, 75, 71, 77]
+        }]
+    });
+});
